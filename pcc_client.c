feef42fa214c1a5b47ed5672bbef703a1c9a1e23
idg7 <idg7722@gmail.com>
2023-01-06 23:44:38 +0200

    Finished implementing client Fixed server errors Untested

---
+++ pcc_client.c
@@ -0,0 +1,146 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <assert.h>
+
+#define MAX_BUFFER_SIZE 1024 * 1024
+
+
+/// @brief Send contents of file to server in chunks
+/// @param sockfd FD for the socket
+/// @param file_path Path to the file to send
+void send_file(int sockfd, char * file_path) {
+    FILE * read_file;
+    uint32_t N = 0, bytes_read = 0, bytes_sent = 0, curr_buffer_size = MAX_BUFFER_SIZE;
+    int status = -1;
+    char * buffer;
+
+    // open file
+    read_file = fopen(file_path, "r");
+    if (read_file == NULL) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        exit(1);
+    }
+
+    // get file size
+    fseek(read_file, 0, SEEK_END);
+    N = (uint32_t)ftell(read_file);
+    fseek(read_file, 0, SEEK_SET);
+
+    // send file size
+    status = write(sockfd, &N, sizeof(N));
+    if (status < 0) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        exit(1);
+    }
+
+    // send file contents iteratively
+    while (N > 0) {
+        // set buffer size
+        if (N < MAX_BUFFER_SIZE) {
+            curr_buffer_size = N;
+        }
+
+        // create buffer
+        buffer = malloc(sizeof(char) * curr_buffer_size);
+        if (buffer == NULL) {
+            fprintf(stderr, "%s\n", strerror(errno));
+            exit(1);
+        }
+        
+        // read buffer
+        bytes_read = fread(buffer, sizeof(char), curr_buffer_size, read_file);
+        if (bytes_read != curr_buffer_size) {
+            fprintf(stderr, "%s\n", strerror(errno));
+            exit(1);
+        }
+
+        // send to server
+        bytes_sent = write(sockfd, buffer, bytes_read);
+        if (bytes_sent != bytes_read) {
+            fprintf(stderr, "%s\n", strerror(errno));
+            exit(1);
+        }
+        free(buffer);
+        N -= bytes_sent;
+    }
+    fclose(read_file);
+}
+
+/// @brief Get from the server the number of printable characters in the file
+/// @param sockfd FD for the socket
+void get_printable_count(int sockfd) {
+    uint32_t readable_count = 0;
+    int bytes_read = 0;
+    bytes_read = read(sockfd, &readable_count, sizeof(readable_count));
+    
+    if (bytes_read != sizeof(readable_count)) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        exit(1);
+    }
+    printf("%d\n", readable_count);
+}
+
+
+/// @brief Connecting to server
+/// @param server_ip The server IP
+/// @param server_port The server port
+/// @return the socket FD
+int connect_to_server(char * server_ip, uint32_t server_port) {
+    struct sockaddr_in serv_addr;
+    int sockfd = 0, status = -1;
+
+    // setup server address
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(server_port);
+    serv_addr.sin_addr.s_addr = inet_pton(server_ip);
+
+    // create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0) {
+        printf("%s\n", strerror(errno));
+        exit(1);
+    }
+
+    // connect to server
+    status = connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
+    if(status < 0) {
+        printf("%s\n", strerror(errno));
+        exit(1);
+    }
+
+    return sockfd;
+}
+
+int main(int argc, char** argv) {
+    char* server_ip, * file_path;
+    uint32_t server_port;
+    int sockfd = 0;
+
+    if (argc != 4) {
+        fprintf(stderr, "%s\n", strerror(EINVAL));
+        exit(1);
+    }
+
+    server_ip = argv[1];
+    server_port = atoi(argv[2]);
+    file_path = argv[3];
+
+    sockfd = connect_to_server(server_ip, server_port);
+
+    send_file(sockfd, file_path);
+
+    get_printable_count(sockfd);
+    
+    close(sockfd);
+
+    return 0;
+}
--- pcc_server.c
+++ pcc_server.c
@@ -14,11 +14,7 @@
 #define PRINT_CHAR_MAX 126
 #define MAX_BUFF_LEN 1024 * 1024
 
-struct pcc_total {
-    uint32_t char_count[PRINT_CHAR_MAX - PRINT_CHAR_MIN + 1];
-}
-
-static struct pcc_total pcc_global;
+static uint32_t pcc_total[PRINT_CHAR_MAX - PRINT_CHAR_MIN + 1];
 
 static uint8_t continue_scan = 1;
 
@@ -26,30 +22,36 @@ void signal_handler(int signum) {
     continue_scan = 0;
 }
 
-/// @brief Scanning a buffer for readable characters, and updating the total count
+/// @brief Scanning a buffer for readable characters, and updating the pcc_session parameters
 /// @param buff Buffer full of input from the client
 /// @param buff_size Size of the buffer (i.e. number of bytes)
-/// @return count of total readable characters in the buffer
-int scan_buffer(uint8_t* buff, uint32_t buff_size) {
+/// @param pcc_session Array of counters for the current session
+void scan_buffer(uint8_t* buff, uint32_t buff_size, uint32_t* pcc_session) {
     uint32_t i, readable_count = 0;
     for (i = 0; i < buff_size; i++) {
         if ((PRINT_CHAR_MIN <= buff[i]) && (buff[i] <= PRINT_CHAR_MAX)) {
             readable_count++;
-            pcc_global->char_count[buff[i] - PRINT_CHAR_MIN]++;
+            pcc_session[buff[i] - PRINT_CHAR_MIN]++;
         }
     }
-
-    return readable_count;
 }
 
 /// @brief Scanning the entire input from a client
 /// @param connfd FD for the connection with the client
 /// @param N Total number of bytes to read from the client
-/// @return Total number of printable characters in the input
-int scan_input(int connfd, int N) {
-    uint32_t buff_size = MAX_BUFF_LEN, bytes_read = 0, total_printable = 0;
-    uint8_t read_buff;
+/// @return Pointer to the array of counters for the current session. On error, returns NULL
+uint32_t * scan_input(int connfd, int N) {
+    uint32_t buff_size = MAX_BUFF_LEN, bytes_read = 0, total_printable = 0, i;
+    uint32_t * pcc_session;
+    uint8_t * read_buff;
     
+    pcc_session = malloc(sizeof(uint32_t) * (PRINT_CHAR_MAX - PRINT_CHAR_MIN + 1));
+    if (pcc_session == NULL) {
+        fprintf(stderr, "%s\n", stderror(errno));
+        exit(1);
+    }
+    memset(pcc_session, 0, sizeof(uint32_t) * (PRINT_CHAR_MAX - PRINT_CHAR_MIN + 1));
+
     // As long as there bytes in the input
     while(N > 0) {
         // Set buffer size
@@ -58,23 +60,35 @@ int scan_input(int connfd, int N) {
         }
         // Read the buffer
         read_buff = malloc(buff_size);
-        bytes_read = read(connfd, &read_buff, buff_size);
-        if (bytes_read != buff_size) {
+        if (read_buff == NULL) {
             fprintf(stderr, "%s\n", stderror(errno));
+            free(pcc_session);
+            exit(1);
+        }
+        bytes_read = read(connfd, read_buff, buff_size);
+        if (bytes_read != buff_size) {
+            // If there was a TCP error we skip the current transaction
+            fprintf(stderr, "%s\n", stderror(errno));
+            free(read_buff);
+            free(pcc_session);
+
+            return NULL;
         }
         // Scan the buffer
-        total_printable += scan_buffer(read_buff, buff_size);
+        scan_buffer(read_buff, buff_size);
+        free(read_buff);
         N -= bytes_read;
     }
-    return total_printable;
+
+    return pcc_session;
 }
 
 /// @brief Server loop, for an entire transaction with a client
 /// @param listenfd FD for the socket to listen to for connections
-/// @return 
 void server_loop(int listenfd) {
     int connfd = -1;
-    uint32_t N = 0, transaction_bytes = 0, total_printable = 0;
+    uint32_t N = 0, transaction_bytes = 0, total_printable = 0, i;
+    uint32_t * pcc_session;
     
     while(continue_scan) {
 
@@ -90,25 +104,50 @@ void server_loop(int listenfd) {
         // Get N
         transaction_bytes = read(connfd, &N, sizeof(N));
         if (transaction_bytes != sizeof(N)) {
+            // If there was a TCP error we skip the current transaction
             fprintf(stderr, "%s\n", stderror(errno));
             continue;
         }
         N = ntohl(N);
         
         // Read buffers from the input iteratively
-        total_printable = scan_input(connfd, N);
+        pcc_session = scan_input(connfd, N);
+
+        if (pcc_session == NULL) {
+            // If there was a TCP error we skip the current transaction
+            continue;
+        }
+        // Update the total count
+        for (i = 0; i < (PRINT_CHAR_MAX - PRINT_CHAR_MIN + 1); i++) {
+            total_printable += pcc_session[i];
+        }
 
         // Send the total count to the client
         total_printable = htonl(total_printable);
         transaction_bytes = write(connfd, &total_printable, sizeof(total_printable));
         if (transaction_bytes != sizeof(total_printable)) {
             fprintf(stderr, "%s\n", stderror(errno));
+            free(pcc_session);
             if ((errno != ETIMEDOUT) && (errno != ECONNRESET) && (errno != EPIPE)) {
                 exit(1);
             }
+        } else {
+            // If everything went well, update the global count
+            for (i = 0; i < (PRINT_CHAR_MAX - PRINT_CHAR_MIN + 1); i++) {
+                pcc_total[i] += pcc_session[i];
+            }
+            free(pcc_session);
         }
-        
+
         close(connfd);
+    }
+}
+
+/// @brief Print the total count of characters
+void print_exit() {
+    uint32_t i;
+    for (i = 0; i < (PRINT_CHAR_MAX - PRINT_CHAR_MIN + 1); i++) {
+        printf("char '%c' : %u times\n", i + PRINT_CHAR_MIN, pcc_total[i]);
     }
 }
 
@@ -127,7 +166,7 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    listenfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
     // We set SO_REUSEADDR to allow the server to restart without waiting for
     // the port to be released by the OS.
@@ -139,7 +178,7 @@ int main(int argc, char** argv) {
 
     memset( &serv_addr, 0, addrsize );
 
-    serv_addr.sin_family = AF_UNIX;
+    serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(port);
 
@@ -158,6 +197,12 @@ int main(int argc, char** argv) {
         .sa_handler = signal_handler
     };
     sigaction(SIGINT, &handler, NULL);
+
+    memset(pcc_total, 0, sizeof(pcc_total));
     
     server_loop(listenfd);
+
+    print_exit();
+    
+    return 0;
 }
